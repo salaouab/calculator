c82d4ec6678c9017fe54dc02d428d10853a2a725
A.Djawad <djawad190@yahoo.fr>
2021-11-01 03:59:31 +0100

    correction

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,11 @@
 public class Calculator {
 
 	public int addition(int a, int b){
-		return a + b
+		return a + b;
 	}
 
 	public double addition(double a, double b){
-                return a + b
+                return a + b;
         }
 
 	public static void main(String[] args) { }
