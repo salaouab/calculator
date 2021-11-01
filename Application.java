c1814c34e8d7d80d3857c75361d4dbe9719e4471
A.Djawad <djawad190@yahoo.fr>
2021-11-01 03:44:09 +0100

    Added Application to call the Calculator

---
+++ Application.java
@@ -0,0 +1,8 @@
+public class Application{
+
+	public static void(String[] args){
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.addiont(1,2)); // should be 3
+		System.out.println(calculator.addiont(1.5,3.2)); // should be 4,7
+	}
+}
