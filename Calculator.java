873e6a12d89030f63d0af465761a8844655a3351
A.Djawad <djawad190@yahoo.fr>
2021-11-01 02:44:14 +0100

    Addition for double values

--- Calculator.java
+++ Calculator.java
@@ -4,5 +4,9 @@ public class Calculator {
 		return a + b
 	}
 
+	public double addition(double a, double b){
+                return a + b
+        }
+
 	public static void main(String[] args) { }
 }
